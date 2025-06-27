756dd1d86dd8204d1c0ae7274636434fbb79afb4
abdulloh <abdullohk1ng987@gmail.com>
2025-06-27 17:18:58 +0500

    nima gap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import About from './Pages/About'
+import Abubakr from './Pages/abubakr'
 
 function App() {
 
   return (
     <>
       <About />
+      <Abubakr/>
     </>
   )
 }
