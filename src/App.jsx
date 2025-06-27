02bb33dd4615cf13d7508334f8ff737aa57aec42
Abubakr70 <abubakrrahimov017gmail.com>
2025-06-27 17:12:52 +0500

    secon commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 
 import './App.css'
+import About from './Pages/About'
 
 function App() {
 
   return (
     <>
-    <h1>Abubakr</h1>
-      <h1>
-        Hello world
-      </h1>
-      <h2>
-        Coder commit
-      </h2>
-      <button>order</button>
+      <About />
     </>
   )
 }
